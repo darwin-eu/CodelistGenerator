42781480879bac22c21ac8a19dec640d95ad23c3
edward-burn <9583964+edward-burn@users.noreply.github.com>
2022-04-06 00:59:07 +0200

    tweak

--- R/get_candidate_codes.R
+++ R/get_candidate_codes.R
@@ -28,10 +28,6 @@
 #'untar(xzfile(system.file("sqlite", "cdm.tar.xz", package = "Eunomia"), open = "rb"),
 #'        exdir =  tempdir())
 #'db <- DBI::dbConnect(RSQLite::SQLite(), paste0(tempdir(),"\\cdm.sqlite"))
-#'vocabulary_database_schema<-"main"
-#'get_candidate_codes(keywords="asthma",
-#'                    db=db,
-#'                    vocabulary_schema = "main")
 #'get_candidate_codes(keywords="asthma",
 #'                    search.synonyms=TRUE,
 #'                              fuzzy.match=TRUE,
