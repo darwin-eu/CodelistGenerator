0eaac807b51abd389ddc5f6c8e9d231c4fa3de2a
edward-burn <9583964+edward-burn@users.noreply.github.com>
2022-08-25 10:47:15 +0200

    tidyWords as internal function

--- R/getCandidateCodes.R
+++ R/getCandidateCodes.R
@@ -784,6 +784,26 @@ if (searchInSynonyms == TRUE |
 
 
 # helper functions for main getCandidateCodes function
+tidyWords <- function(words) {
+  errorMessage <- checkmate::makeAssertCollection()
+  checkmate::assertVector(words, add = errorMessage)
+  checkmate::reportAssertions(collection = errorMessage)
+
+  # to avoid invalid UTF-8 error
+  Encoding(words) <- "latin1"
+
+  # some generic formatting
+  workingWords <- trimws(words)
+  workingWords <- stringr::str_replace_all(workingWords, "-", " ")
+  workingWords <- stringr::str_replace_all(workingWords, "[[:punct:]]", "")
+  workingWords <- stringr::str_remove_all(workingWords, "[^[\\da-zA-Z ]]")
+  workingWords <- stringr::str_remove_all(workingWords, "[^\x01-\x7F]+")
+  workingWords <- stringr::str_to_lower(workingWords)
+  workingWords <- trimws(workingWords)
+
+  workingWords
+}
+
 getExactMatches <- function(words,
                             conceptDf) {
 
