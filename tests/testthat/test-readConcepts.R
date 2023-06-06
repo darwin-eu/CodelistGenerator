caa3f8e7261f4e558aef038f5d33e0a142f4e121
catalamarti <marticatalasabate@gmail.com>
2023-06-06 14:49:58 +0100

    adapt to redshift database

--- tests/testthat/test-readConcepts.R
+++ tests/testthat/test-readConcepts.R
@@ -1,5 +1,18 @@
 test_that("test inputs", {
-  cdm <- mockDrugUtilisation(connectionDetails)
+  testthat::skip_if(Sys.getenv("CDM5_REDSHIFT_DBNAME") == "")
+
+
+  db <-  DBI::dbConnect(RPostgres::Redshift(),
+                        dbname   = Sys.getenv("CDM5_REDSHIFT_DBNAME"),
+                        host     = Sys.getenv("CDM5_REDSHIFT_HOST"),
+                        port     = Sys.getenv("CDM5_REDSHIFT_PORT"),
+                        user     = Sys.getenv("CDM5_REDSHIFT_USER"),
+                        password = Sys.getenv("CDM5_REDSHIFT_PASSWORD"))
+
+
+  cdm <- CDMConnector::cdm_from_con(con = db,
+                                    cdm_schema = "cdmv531",
+                                    write_schema = "public")
   expect_error(readConceptList())
   expect_error(readConceptList(cdm = cdm))
   expect_error(readConceptList(cdm = cdm, path = 1))
@@ -10,5 +23,6 @@ test_that("test inputs", {
   expect_true(typeof(x) == "list")
   expect_true(all(names(x) %in% c("influenza", "acetaminophen")))
   expect_true(x$influenza == 4266367)
-  expect_true(all(x$acetaminophen %in% c(1125315, 43135274, 2905077, 1125360)))
+  expect_true(all(1125315 %in% x$acetaminophen))
+  expect_true(length(x$acetaminophen) > 1)
 })
